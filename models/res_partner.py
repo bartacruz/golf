4274a9633a51b98fb6d17ae705eb77b07b6b4a49
Julio Santa Cruz <bartacruz@gmail.com>
2021-12-14 10:45:06 -0300

    Player cards stat button

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import _,fields, models
 
 
 class ResPartner(models.Model):
@@ -11,11 +11,26 @@ class ResPartner(models.Model):
     golf_handicap = fields.Integer(
         string='handicap',
     )
-    
+    golf_card_ids = fields.One2many(
+        'golf.card',
+        'player_id',
+        string=_('Cards')
+    )
+    golf_card_count = fields.Integer(compute = '_golf_count_cards')
+
     golf_membership = fields.Many2one(
         string='Membership',
         comodel_name='product.template',
         ondelete='restrict',
     )
 
+    def _golf_count_cards(self):
+        for rec in self:
+            rec.golf_card_count = len(rec.golf_card_ids)
     
+    def action_open_golf_cards(self):
+        for rec in self:
+            action = self.env.ref("golf.action_golf_card_act_window").read()[0]
+            action["context"] = {}
+            action["domain"] = [("id", "in", rec.golf_card_ids.ids)]
+            return action
