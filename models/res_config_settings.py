c56522eec2b2d049d4479066baa65a4578f68c91
Julio Santa Cruz <bartacruz@gmail.com>
2022-09-21 12:28:49 -0300

    importación de torneos y jugadores desde aag

---
+++ models/res_config_settings.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import _,fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = ['res.config.settings']
+
+    golf_club_id = fields.Char(config_parameter='golf.club_id')
+    golf_default_product = fields.Many2one("product.product",string=_("Default product"), config_parameter='golf.default_product')
+    golf_default_identification_type = fields.Many2one("l10n_latam.identification.type", config_parameter='golf.default_identification_type')
+    golf_default_responsibility = fields.Many2one("l10n_ar.afip.responsibility.type", config_parameter='golf.default_responsibility')
+
+    golf_members_pricelist = fields.Many2one('product.pricelist', config_parameter='golf.members_pricelist')
+    
